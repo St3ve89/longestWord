d576ff05d0af26d45e1e388dce119f610fe89806
St3ve89 <istvan.acs89@gmail.com>
2018-01-24 19:50:22 +0000

    add for loop

--- Find the Longest Word in a String.js
+++ Find the Longest Word in a String.js
@@ -1,7 +1,15 @@
 function findLongestWord(str) {
-    var longesWord = 0;
-    str = str.split(" ");
-    return str;
+    var longesWordLength = 0;
+    var longestWord;
+    // Split string into array by space
+    var arr = str.split(" ");
+    // make for loop to go trough each one 
+    for (var i = 0; i < arr.length; i++) {
+        console.log(i);
+    }
+    // count the number of letters on each one
+    //if the world longer then the longestwordlength then put the word into the var
+    return longestWord;
 }
 
 console.log(
