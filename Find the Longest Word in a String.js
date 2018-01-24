30ee8a456da1b2dcdb0e0f4acdd3689d4a15b7a6
St3ve89 <istvan.acs89@gmail.com>
2018-01-24 19:46:50 +0000

    initial commit

---
+++ Find the Longest Word in a String.js
@@ -0,0 +1,9 @@
+function findLongestWord(str) {
+    var longesWord = 0;
+    str = str.split(" ");
+    return str;
+}
+
+console.log(
+    findLongestWord("The quick brown fox jumped over the lazy dog")
+);
